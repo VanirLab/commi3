493acaca21e1c18c0f0ac09fe999e6c77f738149
Chris Pro <prokic720x@gmail.com>
2019-04-16 10:34:49 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,10 +1,11 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # encoding: UTF-8
 
 """
 This file is part of Commi3 Vanir Project.
 Copyright (c) 2019.
- 
+
+
 For more see the file 'readme/COPYING' for copying permission.
 """
 
